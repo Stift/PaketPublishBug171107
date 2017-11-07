cd7137e3277f719eb319e3b48e4c8787b34defe2
Christian Fiebrig <fiebrig@freenet.de>
2017-11-07 10:47:01 +0100

    init

---
+++ src/noPaket/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CreateFirmwareRolloutPhases
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+
+    }
+}
